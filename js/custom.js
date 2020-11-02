00d6edf5ddc8d1b1070cca8fba907ee0f294bbaf
Mr.Hợp <hopvq89@gmail.com>
2020-11-02 17:02:31 +0700

    update scroll

--- js/custom.js
+++ js/custom.js
@@ -48,6 +48,7 @@
         autoPlay: false,
         autoplayTimeout: 5000,
         slideSpeed: 500,
+        autoHeight: false,
         items: 1,
         navigation: true,
         itemsDesktop: [1199, 1],
@@ -85,6 +86,38 @@
          $('#post').css({"maxHeight":topNew-40});
          $('.ads').css({"maxHeight":topNew});
        }
+       function carouselNormalization() {
+
+        window.heights = [], //create empty array to store height values
+        window.tallest; //create variable to make note of the tallest slide
+
+        function normalizeHeights() {
+            jQuery('.ni-content').each(function() { //add heights to array
+                window.heights.push(jQuery(this).outerHeight());
+            });
+            window.tallest = Math.max.apply(null, window.heights); //cache largest value
+            jQuery('.ni-content').each(function() {
+                jQuery(this).css('min-height',tallest + 'px');
+            });
+        }
+        normalizeHeights();
+
+        jQuery(window).on('resize orientationchange', function () {
+
+            window.tallest = 0, window.heights.length = 0; //reset vars
+            jQuery('.ni-content').each(function() {
+                jQuery(this).css('min-height','0'); //reset min-height
+            });
+
+            normalizeHeights(); //run it again
+
+        });
+
+    }
+
+    jQuery( document ).ready(function() {
+        carouselNormalization();
+    });
 });
 var BrowserDetect = {
         init: function () {
